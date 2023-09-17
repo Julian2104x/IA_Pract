8189066510526840a5a57f8acd140082ef132a30
Julian2104x <julian2104x@gmail.com>
2023-09-17 00:35:55 -0600

    Final de las listas

---
+++ _16._sort()_sorted().py
@@ -0,0 +1,4 @@
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón',
+'lila', 'negro', 'rosa', 'blanco', 'naranja']
+colores.sort(reverse=True) #True siempre con T
+print(colores)
---
+++ _17._len().py
@@ -0,0 +1,3 @@
+colores = ['rojo', 'azul', 'verde', 'amarillo']
+longitud_colores = len(colores) #cuenta elementos de la lista
+print(len(colores))
