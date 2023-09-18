983a4f1e7cec71615ff7f1494ec7c1527dcb4c1e
Julian2104x <julian2104x@gmail.com>
2023-09-18 00:31:52 -0600

    Update _20._if_else.py

--- _20._if_else.py
+++ _20._if_else.py
@@ -1,6 +1,6 @@
 color = 'rojo'
 
-if color == rojo
-print("El color es rojo.")
-else
-print("El color no es rojo.")
+if color == "rojo":
+    print("El color es rojo.")
+else:
+    print("El color no es rojo.")
